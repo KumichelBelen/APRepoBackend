1adf2c3f91640d4fb1efc80db474b50a3489b4cc
gifuentes92 <isa2_7_92@hotmail.com>
2023-03-24 23:16:18 -0300

    división Back

--- frontend/src/app/entidades/provincia.ts
+++
@@ -1,9 +0,0 @@
-export class provincia{
-    idProvincia:number;
-    Nombre:string;
-
-    constructor(idProvincia:number,Nombre:string){
-        this.idProvincia=idProvincia;
-        this.Nombre=Nombre;
-    }
-}
