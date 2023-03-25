71fc95612c67e4489268fb09d832f9c712bf4bbc
gifuentes92 <isa2_7_92@hotmail.com>
2023-03-25 05:14:06 -0300

    correccion error build

--- src/test/java/com/myApi/Springboot/SpringbootApplicationTests.java
+++ src/test/java/com/myApi/Springboot/SpringbootApplicationTests.java
@@ -3,7 +3,7 @@ package com.myApi.Springboot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringbootApplicationTests.class)
 class SpringbootApplicationTests {
 
 	@Test
